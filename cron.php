89b93b8b1aa5ce52d6297a357acd2c22293f8818
Marten Tacoma <marten@eupeodes.nl>
2017-04-25 17:23:35 +0200

    Now possible to trigger irc and twitter bot when adding dow externaly

--- cron.php
+++ cron.php
@@ -12,6 +12,17 @@ $ircBot = new \cron\IrcBot();
 
 $ircBot->cron();
 
+//option to trigger dailyCoords from the db
+$db = \lib\Db::getInstance();
+$q = $db->prepare('SELECT val FROM conf WHERE field=\'trigger_post_date\'');
+$q->execute();
+$trigger_post_date = $q->fetch(PDO::FETCH_OBJ)->val;
+if(!is_null($val)){
+	$twitter->dailyCoords($val);
+	$ircBot->dailyCoords($val);
+	$db->prepare('UPDATE conf SET val=null WHERE field=\'trigger_post_date\'')->execute();
+}
+
 //do all the daily stuff if new dow is available
 $dowDate = \model\Date::max();
 if(\model\Dow::getNew()){
