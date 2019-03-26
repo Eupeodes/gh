a8af6d635b515d2e7b21a2fb78e4b5a72df639aa
Marten Tacoma <marten@martentacoma.nl>
2019-03-26 21:08:40 +0100

    Allow version update using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp')
 	, concat = require('gulp-concat')
 	, rename = require('gulp-rename')
-	, sourcemaps = require('gulp-sourcemaps')
 	, uglify = require('gulp-uglify')
-	, sass = require('gulp-sass');
-
+	, sass = require('gulp-sass')
+	, bump = require('gulp-bump')
+	, fs = require('fs')
+	, insert = require('gulp-insert')
+;
 function css(){
 	return gulp.src(['scss/*.scss', '!scss/[a-z]_*.scss'], {sourcemaps: true})
 		.pipe(sass({outputStyle: 'compressed'}))
@@ -27,6 +29,39 @@ function mobileJs(){
 function mapJs(){
 	return js(['javascript/base.js', 'javascript/map.js'], 'map');
 }
+
+
+function changelogMsg(version){
+	var msg = '* REMEMBER TO UPDATE CHANGELOG ';
+	if(version !== undefined){
+		msg = msg + 'FOR v'+version;
+	}
+	msg = msg + ' *';
+	console.log('*'.repeat(msg.length));
+	console.log(msg);
+	console.log('*'.repeat(msg.length));
+}
+function changelog(){
+	var version = getPackageJsonVersion();
+	changelogMsg(version);
+	var date = new Date();
+	return gulp.src('CHANGELOG.md')
+		.pipe(insert.prepend('## v'+ version + ' - ' +date.toISOString().substring(0, 10)+'\n\n\n'))
+		.pipe(gulp.dest('./'));
+};
+
+function bumpVersion(lvl){
+	if(lvl !== 'prerelease'){
+		changelogMsg();
+	}
+	return gulp.src(['package.json', 'package-lock.json'])
+		.pipe(bump({type: lvl}))
+		.pipe(gulp.dest('./'));
+}
+
+function getPackageJsonVersion () {
+	return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+};
 
 gulp.task('css', function(){
 	return css();
@@ -54,3 +89,24 @@ gulp.task('watch', function(){
 		return mapJs();
 	});
 });
+
+gulp.task('bump:test', function(){
+	return bumpVersion('prerelease');
+});
+
+gulp.task('bump:patch', function(){
+	return bumpVersion('patch');
+});
+
+gulp.task('bump:minor', function(){
+	return bumpVersion('minor');
+});
+
+gulp.task('bump:major', function(){
+	return bumpVersion('major');
+});
+
+gulp.task('changelog', function(){
+	return changelog();
+});
+
