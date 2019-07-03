9a8a50e7e96a91be374af457d80d660f2544c281
Marten Tacoma <marten@martentacoma.nl>
2019-07-03 15:34:57 +0200

    Changed coordinate precision to 5 decimals (~ 1 meter)

--- model/Hash.php
+++ model/Hash.php
@@ -10,17 +10,18 @@ class Hash{
 	private $date;
 	public $lat;
 	public $lng;
+	private $decimals = 5;
 	
 	public function __construct(DateTime $date, Dow $dow, $global = false){
 		$this->date = $date->format('Y-m-d');
 		$md5 = md5($this->date."-".$dow->dow);
 		list($lat,$lng) = str_split($md5,16);
 		if($global){
-			$this->lat = $this->hex2dec($lat) * 180 - 90;
-			$this->lng = $this->hex2dec($lng) * 360 - 180;
+			$this->lat = round($this->hex2dec($lat) * 180 - 90, $this->decimals);
+			$this->lng = round($this->hex2dec($lng) * 360 - 180, $this->decimals);
 		} else {
-			$this->lat = $this->hex2dec($lat);
-			$this->lng = $this->hex2dec($lng);
+			$this->lat = round($this->hex2dec($lat), $this->decimals);
+			$this->lng = round($this->hex2dec($lng), $this->decimals);
 		}
 	}
 	
